297f2b00616404cbcf70e27c747864777acda240
AdrianToscanoDev <adrian.toscano.developer@gmail.com>
2025-03-05 19:46:54 -0800

    added button display content functionality

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -26,6 +29,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -51,6 +57,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizcard() {
+
+    // keeps track of when the button to show portfolio
+    val buttonClickedState = remember {
+        mutableStateOf(value = false)
+    }
 
     // main background
     Surface(modifier = Modifier
@@ -86,25 +97,34 @@ fun CreateBizcard() {
                 CreateUserDetails()
 
                 // portfolio button
-                CreatePortfolioButton()
+                CreatePortfolioButton(buttonClickedState)
             }
         }
     }
 }
 
+// UI for the clickable portfolio button.
 @Composable
-private fun CreatePortfolioButton() {
+private fun CreatePortfolioButton(buttonClickedState: MutableState<Boolean>) {
     Button(
         modifier = Modifier
             .padding(1.dp),
         onClick = {
-
+            buttonClickedState.value = !buttonClickedState.value
         }) {
         Text(
             modifier = Modifier,
             text = "Portfolio",
             fontSize = 17.sp
         )
+    }
+
+    // display content when the button is clicked.
+    if (buttonClickedState.value) {
+        Content()
+    }
+    else {
+        Box() { }
     }
 }
 
@@ -168,7 +188,37 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview
+@Composable
+fun Content() {
+    Box(modifier = Modifier
+        .padding(5.dp)
+        .fillMaxHeight()
+        .fillMaxWidth()
+    ) {
+        Surface (
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn{
+        items(data) { item ->
+            Text(item)
+        }
+    }
+}
+
+// @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetBizcardTheme {
